ea3e220f1cd1762a98d9f729654d6504bafebb18
Pacifique Twagirayesu <56336540+paccy-ops@users.noreply.github.com>
2021-01-17 21:54:03 +0800

    jquey

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,38 @@
 // smoth scrolling
-$('#navbar a, .btn').on('click',function(event){
-         if(this.hash !== ''){
-                  event.preventDefault();
-                  const hash = this.hash;
-                  
-                  $('html,body').animate({
-                          scrollTop:$(hash).offset().top -80 
-                  },
-                  800
-                  );
-         }
-})
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+
+    $('html,body').animate(
+      {
+        scrollTop: $(hash).offset().top - 80,
+      },
+      800
+    );
+  }
+});
+
+$(document).ready(function () {
+  //   $('#nav_hover').click(function () {
+  //     $('checkbtn').css('visibility', 'visible');
+  //   });
+  $('ul').click(function () {
+    $('#navbar ul').css('visibility', 'hidden');
+  });
+  $('#icon').click(function () {
+    $('#navbar ul').css('visibility', 'visible');
+  });
+  //   $('#navbar').mouseleave(function () {
+  //     $(' #navbar').css('visibility', 'hidden');
+  //   });
+});
+
+// function mobileMenu() {
+//   if (document.getElementById('#navbar ul ').style.display == 'block') {
+//     document.getElementById('#navbar ul ').style.display = 'none';
+//   } else {
+//     document.getElementById('#navbar ul ').style.display == 'block';
+//   }
+//   document.getElementById('#navbar ul ').addEventListener('click', mobileMenu);
+// }
